e56b8f2d8f06d911f872771528b3db0a73708d44
Molo.gg <dadada03800@gmail.com>
2026-04-14 16:35:45 +0400

    added coin, made github public

---
+++ README.md
@@ -0,0 +1,3 @@
+kubejs folder of my minecraft forge 1.20.1 modpack Molo's Ultimate Progression
+do whatever u want with all that shit
+-molo 14.04.2026
---
+++ startup_scripts/coins.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', event => {
+    event.create('coin_tier_0')
+        .displayName('Coin Tier 0')
+        .texture('mupt:item/coins/coin_tier_0')
+})
